43abb383e3fa2633a7eaff52c30fb2a80da97014
Md Ratul Bhuiyan <ratulbhuiyan001@gmail.com>
2024-08-06 19:29:56 +0600

    permutation adding in baclTracking file

--- backTracking.java
+++ backTracking.java
@@ -1,4 +1,17 @@
 public class backTracking {
+    public static void permutation(String str,String ans){
+        //base
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+        //recursion
+        for(int i=0;i<str.length();i++){
+            char crnt = str.charAt(i);
+            String Newstr = str.substring(0,i) + str.substring(i+1);
+            permutation(Newstr,ans+crnt);
+        }
+    }
     public static void subSet(String str,String ans,int i){
         //base
         if(i==str.length()){
@@ -18,5 +31,6 @@ public class backTracking {
     public static void main(String args[]){
         String str = "Hello";
         subSet(str,"",0);
+        permutation(str,"");
     }
 }
